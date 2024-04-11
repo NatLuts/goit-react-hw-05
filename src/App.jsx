54594d4e6bacdc8233a05ae6d783e81c162fdde3
NatLuts <lutsenko.natusiia@gmail.com>
2024-04-11 21:41:25 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
